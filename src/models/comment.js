b3df21be247c25d1e8c4bd1b35e799a315420cb8
vigbi <bishoyibighneswar@gmail.com>
2024-12-01 01:07:29 +0530

    Updated comment model

--- src/models/comment.js
+++ src/models/comment.js
@@ -1,14 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 const commentSchema = new mongoose.Schema({
     content: {
         type: String,
         required: true,
     },
-    userEmail: {
-        type: String
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
     },
+    onModel: {
+        type: String,
+        required: true,
+        enum: ['Tweet', 'Comment']
+    },
+    commentable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        refPath: 'onModel'
+    }
 }, {timestamps: true});
 
 const Comment = mongoose.model('Comment', commentSchema);
-export default Comment;
